e61355cec8419c2a477b68a457cef5be5ebce9bc
skim13waketech <56847449+skim13waketech@users.noreply.github.com>
2020-05-31 20:36:03 -0400

    Upload

---
+++ public/app/routes/dashboard/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+    'route': '/dashboard',
+    'controller': function() {
+    },
+    'templateUrl': '/app/routes/dashboard/template.html',
+    'resolve': {}
+};
